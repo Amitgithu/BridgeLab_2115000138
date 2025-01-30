d7b68d24e386759479d39f29723b71566e4c1c50
Amit Kumar <amit909563@gmail.com>
2025-01-30 12:06:48 +0530

    Modified: RemoveDuplicatesFromString programs

--- String_Programs/RemoveDuplicatesFromString.java
+++ String_Programs/RemoveDuplicatesFromString.java
@@ -10,14 +10,14 @@ public class RemoveDuplicatesFromString {
     }
 
     private static String removeDuplicates(String str) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
+        String modifiedString = "";
+        for(int i = 0;i < str.length();i++){
             char ch = str.charAt(i);
-            if (sb.indexOf(String.valueOf(ch)) == -1) {
-                sb.append(ch);
+            if(modifiedString.indexOf(ch) == -1){
+                modifiedString += ch;
             }
         }
-        return sb.toString();
+        return modifiedString;
     }
     
 }
