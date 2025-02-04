514cfc4d8442d88aaf37edb38338773447a0a8fc
Amit Kumar <amit909563@gmail.com>
2025-02-04 14:32:01 +0530

    Added: Library Management System program

---
+++ Book.java
@@ -0,0 +1,64 @@
+public class Book{
+    private String author;
+    private String title;
+    private final String isbn;
+    private static String libraryName = "Central Library";
+
+    public Book(){
+        this.author = "Anonymous";
+        this.title = "";
+        this.isbn = "";
+    }
+
+    public Book(String author, String title, String isbn){
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public String getAuthorName(){
+        return this.author;
+    }
+
+    public void setAuthorName(String newAuthorName){
+        this.author = newAuthorName;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+
+    public String getIsbnNumber(){
+        return this.isbn;
+    }
+
+    public static String displayLibraryName(){
+        return this.libraryName;
+    }
+
+    public void displayDetails(){
+        System.out.println("Library Name is : " + this.libraryName);
+        System.out.println("Author Name of Book is : " + this.author);
+        System.out.println("Title of the Book is : " + this.title);
+        System.out.println("ISBN Number of Book is : " + this.isbn);
+
+    }
+}
+
+class Main{
+    public static void main(String []args){
+        Book book1 = new Book("Amit Kumar", "Java Programming", "9872-8463-2849");
+        if(isInstanceOf(book1)){
+            book1.displayDetails();
+        }
+
+        Book book2 = new Book("Mohit Agarwal", "Lets learn with C", "8756-3647-2947");
+        if(isInstanceOf(book2)){
+            book2.displayDetails();
+        }
+    }
+}
