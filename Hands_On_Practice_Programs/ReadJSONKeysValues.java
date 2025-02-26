f65cfe61512de9fb24b3892ec1fd2a8598d29625
Amit Kumar <amit909563@gmail.com>
2025-02-26 23:19:04 +0530

    Fixes: Some Issue in Programs

--- Hands_On_Practice_Programs/ReadJSONKeysValues.java
+++ Hands_On_Practice_Programs/ReadJSONKeysValues.java
@@ -1,19 +1,23 @@
 import org.json.JSONObject;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Scanner;
 
-public class ReadJSONFile {
+public class ReadJSONKeysValues {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String filePath = "user.json"
+        String filePath = "data.json";
 
         try {
             String content = new String(Files.readAllBytes(Paths.get(filePath)));
             JSONObject jsonObject = new JSONObject(content);
 
-            System.out.println("Name: " + jsonObject.getString("name"));
-            System.out.println("Email: " + jsonObject.getString("email"));
+            Iterator<String> keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                System.out.println(key + ": " + jsonObject.get(key));
+            }
         } catch (Exception e) {
             System.out.println("Error reading JSON file: " + e.getMessage());
         }
